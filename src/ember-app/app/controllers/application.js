fe56ccda52d915782077843565b82f1f8ef45fea
Flexberry-man <mail@flexberry.net>
2023-12-18 21:27:24 +0000

    Flexberry code generation

--- src/ember-app/app/controllers/application.js
+++ src/ember-app/app/controllers/application.js
@@ -32,37 +32,37 @@ export default Controller.extend({
               link: 'i-i-s-itogovaya-tochno-номенклатура-l',
               caption: i18n.t('forms.application.sitemap.автоматизация-закупок.справочник.i-i-s-itogovaya-tochno-номенклатура-l.caption'),
               title: i18n.t('forms.application.sitemap.автоматизация-закупок.справочник.i-i-s-itogovaya-tochno-номенклатура-l.title'),
-              icon: 'suitcase',
+              icon: 'edit',
               children: null
             }, {
               link: 'i-i-s-itogovaya-tochno-организации-l',
               caption: i18n.t('forms.application.sitemap.автоматизация-закупок.справочник.i-i-s-itogovaya-tochno-организации-l.caption'),
               title: i18n.t('forms.application.sitemap.автоматизация-закупок.справочник.i-i-s-itogovaya-tochno-организации-l.title'),
-              icon: 'table',
+              icon: 'chart bar',
               children: null
             }, {
               link: 'i-i-s-itogovaya-tochno-контрагенты-l',
               caption: i18n.t('forms.application.sitemap.автоматизация-закупок.справочник.i-i-s-itogovaya-tochno-контрагенты-l.caption'),
               title: i18n.t('forms.application.sitemap.автоматизация-закупок.справочник.i-i-s-itogovaya-tochno-контрагенты-l.title'),
-              icon: 'suitcase',
+              icon: 'chart line',
               children: null
             }, {
               link: 'i-i-s-itogovaya-tochno-склады-l',
               caption: i18n.t('forms.application.sitemap.автоматизация-закупок.справочник.i-i-s-itogovaya-tochno-склады-l.caption'),
               title: i18n.t('forms.application.sitemap.автоматизация-закупок.справочник.i-i-s-itogovaya-tochno-склады-l.title'),
-              icon: 'archive',
+              icon: 'folder',
               children: null
             }, {
               link: 'i-i-s-itogovaya-tochno-единицы-измер-l',
               caption: i18n.t('forms.application.sitemap.автоматизация-закупок.справочник.i-i-s-itogovaya-tochno-единицы-измер-l.caption'),
               title: i18n.t('forms.application.sitemap.автоматизация-закупок.справочник.i-i-s-itogovaya-tochno-единицы-измер-l.title'),
-              icon: 'paperclip',
+              icon: 'archive',
               children: null
             }, {
               link: 'i-i-s-itogovaya-tochno-менеджер-l',
               caption: i18n.t('forms.application.sitemap.автоматизация-закупок.справочник.i-i-s-itogovaya-tochno-менеджер-l.caption'),
               title: i18n.t('forms.application.sitemap.автоматизация-закупок.справочник.i-i-s-itogovaya-tochno-менеджер-l.title'),
-              icon: 'list',
+              icon: 'file',
               children: null
             }]
           }, {
@@ -73,13 +73,13 @@ export default Controller.extend({
               link: 'i-i-s-itogovaya-tochno-заказ-пост-l',
               caption: i18n.t('forms.application.sitemap.автоматизация-закупок.документы.i-i-s-itogovaya-tochno-заказ-пост-l.caption'),
               title: i18n.t('forms.application.sitemap.автоматизация-закупок.документы.i-i-s-itogovaya-tochno-заказ-пост-l.title'),
-              icon: 'file',
+              icon: 'paperclip',
               children: null
             }, {
               link: 'i-i-s-itogovaya-tochno-план-закупок-l',
               caption: i18n.t('forms.application.sitemap.автоматизация-закупок.документы.i-i-s-itogovaya-tochno-план-закупок-l.caption'),
               title: i18n.t('forms.application.sitemap.автоматизация-закупок.документы.i-i-s-itogovaya-tochno-план-закупок-l.title'),
-              icon: 'chart bar',
+              icon: 'phone',
               children: null
             }]
           }, {
@@ -90,7 +90,7 @@ export default Controller.extend({
               link: 'i-i-s-itogovaya-tochno-отчет-о-зак-l',
               caption: i18n.t('forms.application.sitemap.автоматизация-закупок.отчеты.i-i-s-itogovaya-tochno-отчет-о-зак-l.caption'),
               title: i18n.t('forms.application.sitemap.автоматизация-закупок.отчеты.i-i-s-itogovaya-tochno-отчет-о-зак-l.title'),
-              icon: 'address card',
+              icon: 'briefcase',
               children: null
             }]
           }]
